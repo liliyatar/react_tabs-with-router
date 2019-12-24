6db070489708657d6c348b8f1d5ed649128657b1
Liliya Taran <liliyataran2017@gmail.com>
2019-12-24 18:10:12 +0200

    Updates

---
+++ precache-manifest.e4a0d85ad1ab18b3cff5b9f03ce6f802.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1f9ebd600d7bd64de93a405cdba5d15",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "9dfd29ae54d1f357a1bc",
+    "url": "/react_tabs-with-router/static/css/main.9bb9e35b.chunk.css"
+  },
+  {
+    "revision": "16ee3b22fa33ea08fb81",
+    "url": "/react_tabs-with-router/static/js/2.32a3fed8.chunk.js"
+  },
+  {
+    "revision": "9dfd29ae54d1f357a1bc",
+    "url": "/react_tabs-with-router/static/js/main.a6445cc7.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
